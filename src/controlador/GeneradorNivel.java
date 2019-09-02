5662addf1cded98be7da817dc49e299ec43d4757
franGomez <fgvolonterio@gmail.com>
2019-09-01 23:16:38 -0300

    Cambios menores

---
+++ src/controlador/GeneradorNivel.java
@@ -0,0 +1,13 @@
+package controlador;
+
+import java.util.List;
+
+public class GeneradorNivel {
+    public GeneradorNivel(int nivel) {
+
+    }
+
+    public void generar(List<GameObject> enemigos) {
+
+    }
+}
--- src/controlador/TableroJuego.java
+++ src/controlador/TableroJuego.java
@@ -2,10 +2,17 @@ package controlador;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class TableroJuego extends JPanel {
 
+    private int nivel;
+
     private Colisionador colisionador;
+    private GeneradorNivel nivelGen;
+    private List<GameObject> objetosMapa;
+
 
     public TableroJuego() {
         super();
@@ -13,6 +20,13 @@ public class TableroJuego extends JPanel {
         this.setLayout(null);
         this.setSize(300, 530);
         this.setBackground(Color.BLACK);
+
+        nivel = 1;
+        nivelGen = new GeneradorNivel(nivel);
+        objetosMapa = new LinkedList<>();
+        nivelGen.generar(objetosMapa);
+
+        colisionador = new Colisionador();
     }
 
     public void gameLoop() {
