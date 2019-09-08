5428aa070a184c716ff59bd74fc47221c282ec9c
RinghettiFranco <54511049+RinghettiFranco@users.noreply.github.com>
2019-09-08 17:36:33 -0300

    Agrega el metodo disparar

--- src/torres/Torre.java
+++ src/torres/Torre.java
@@ -13,4 +13,10 @@ public abstract class Torre extends GameObject {
     public Torre(int vida, int alcance, int impacto, ImageIcon graphic) {
         super(vida, alcance, impacto, graphic);
     }
+    
+    public void disparar(){
+    	//Manejo del colisionador verificando si hay enemigo al alcance o no.
+    	golpe.atacar();
+    }
 }
+
