f1373f220c84c0bab19caa584894f3918d9d4d1d
RinghettiFranco <54511049+RinghettiFranco@users.noreply.github.com>
2019-11-03 16:15:56 -0300

    Reimplementacion generador nivel

--- src/control_logico/GeneradorNivel.java
+++ src/control_logico/GeneradorNivel.java
@@ -6,22 +6,42 @@ import enemigos.*;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.*;
 
 public class GeneradorNivel {
 	
 	public List<GameObject> generar(int nivel){
 		List<GameObject> oleada = new LinkedList();
-		oleada.add(new Chewbaca(25,25));
-		oleada.add(new HanSolo(25,125));
-		oleada.add(new LukeSkywalker(25,225));
+		Random rnd = new Random(System.currentTimeMillis());
+
+		int corrimiento = 900;
+		int distancia = 1200;
+		int auxInicio;
 		for(int i=0;i<(nivel*2);i++){
-			oleada.add(new BobbaFet(800+(i*100),25));
-			oleada.add(new DarthVader(800+(i*100),125));
-			oleada.add(new Soldier(800+(i*100),225));
-			oleada.add(new Stormtrooper(800+(i*100),325));
-			oleada.add(new Stormtrooper(800+(i*100),425));
+			auxInicio = (i==0)?0:1;
+			oleada.add(new Stormtrooper(900+(i*distancia)+(distancia/4)*(rnd.nextInt(3)+1)+rnd.nextInt(50),25));
+			oleada.add(new Stormtrooper(900+(i*distancia)+auxInicio*(distancia/4)*(rnd.nextInt(3)+1)+rnd.nextInt(50),125));
+			oleada.add(new Stormtrooper(900+(i*distancia)+(distancia/4)*(rnd.nextInt(3)+1)+rnd.nextInt(50),225));
+			oleada.add(new Stormtrooper(900+(i*distancia)+(distancia/4)*(rnd.nextInt(3)+1)+rnd.nextInt(50),325));
+			oleada.add(new Stormtrooper(900+(i*distancia)+(distancia/4)*(rnd.nextInt(3)+1)+rnd.nextInt(50),425));
+			corrimiento +=(distancia/4);
 			
 		}
+		corrimiento += distancia;
+		int pos = (rnd.nextInt(4))*100 + 25;
+		oleada.add(new BobbaFet(corrimiento,pos));
+		int inicio = corrimiento;
+		for(int j=0;j<nivel;j++){
+			oleada.add(new Soldier(inicio+(j*distancia)+(distancia/6)*(rnd.nextInt(3)+1)+rnd.nextInt(50),25));
+			oleada.add(new Soldier(inicio+(j*distancia)+(distancia/6)*(rnd.nextInt(3)+1)+rnd.nextInt(50),125));
+			oleada.add(new Soldier(inicio+(j*distancia)+(distancia/6)*(rnd.nextInt(3)+1)+rnd.nextInt(50),225));
+			oleada.add(new Soldier(inicio+(j*distancia)+(distancia/6)*(rnd.nextInt(3)+1)+rnd.nextInt(50),325));
+			oleada.add(new Soldier(inicio+(j*distancia)+(distancia/6)*(rnd.nextInt(3)+1)+rnd.nextInt(50),425));
+			corrimiento += (distancia/4);
+		}
+		corrimiento += distancia*nivel;
+		pos = (rnd.nextInt(4))*100 + 25;
+		oleada.add(new DarthVader(corrimiento,pos));
 		return oleada;
     }
 }
--- src/movimiento/MovimientoEnemigo.java
+++ src/movimiento/MovimientoEnemigo.java
@@ -6,12 +6,12 @@ import java.awt.geom.Point2D;
 public class MovimientoEnemigo extends Movimiento {
 
 	public MovimientoEnemigo(int x,int y) {
-actual=new Point2D.Double(x,y);	
-}
+		actual=new Point2D.Double(x,y);	
+	}
 	
 	
 	public Point2D proximaPosicion() {
-		actual.setLocation(actual.getX()-1,actual.getY());
+		actual.setLocation(actual.getX()-0.65,actual.getY());
 		return actual;
 	}
 
