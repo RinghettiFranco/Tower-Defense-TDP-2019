c2f4507178cff261ddd60c9da0cc0d7eb8631a4a
RinghettiFranco <54511049+RinghettiFranco@users.noreply.github.com>
2019-11-25 11:59:41 -0300

    Update Torre.java

--- src/torres/Torre.java
+++ src/torres/Torre.java
@@ -7,16 +7,22 @@ import control_grafico.GameObject;
 import control_logico.Visitor;
 import enemigos.Enemigo;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.*;
 
 public abstract class Torre extends GameObject {
 
     protected int costo;
     protected int cuentaRegresiva;
+    protected int vidaInicial;
 
     public Torre(int vida, int alcance, int impacto, ImageIcon graphic) {
         super(vida, alcance, impacto, graphic);
         this.puntaje = -this.puntaje;
+        this.vidaInicial=vida;
+        addMouseListener(new Vender());
     }
 
     public void actualizar(){
@@ -45,5 +51,49 @@ public abstract class Torre extends GameObject {
         tableroJuego.delFromObjects(this);
     }
     public void frenar() {}
+    
+    public boolean recibioDmg(){
+    	return !(vidaInicial==this.vida.obtenerVida());
+    }
+    
+    private class Vender implements MouseListener{
+
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if(SwingUtilities.isRightMouseButton(e)){
+				if(!recibioDmg()){
+					inventario.actualizarOro(costo);
+					morir();
+				}else{
+					System.out.print("no se puede vender");
+				}
+			}
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+    	
+    }
 }
 
